ed1117aa3d3abfa4c909880d4605ff6ab8532660
Mac <mac@Macs-Air-2.lan>
2022-11-19 00:48:25 +0100

    second commit

--- AssignmentTemperatureConverter.java
+++ AssignmentTemperatureConverter.java
@@ -7,54 +7,41 @@ public class AssignmentTemperatureConverter {
         System.out.println("-----------------------------------Welcome------------------------------------------");
         System.out.println("---Celsius-Fahrenheit/Fahrenheit-Celsius converter---");
         //initialize boolean variable con to false
-        boolean con = false;
+        boolean convertyes = false;
         do {
             //give to user the choice of conversion (celsius-fahrenheit or fahrenheit-celsius)
             Scanner sc = new Scanner(System.in);
             System.out.println("choose the conversion mode:  \n1- Celsius-Fahrenheit\n2- Fahrenheit-Celsius\n");
-            int choice = sc.nextInt();
+            String choice = sc.nextLine();
             //if the user chooses celsius-fahrenheit he must type 1
-            if (choice == 1) {
-
-                System.out.println("Enter the temperature to convert:");
-
-                Scanner scanner = new Scanner(System.in);
-
-                double c = scanner.nextDouble();
-                double f;
-
-                f = (c * 9 / 5) + 32;
-
-                System.out.println(c + "C ------------------> " + f + "F");
-            }
-            //if the user chooses fahrenheit-celsius he must type 2
-            else if (choice == 2) {
-
-                System.out.println("Enter the temperature to convert:");
-
-                Scanner scanner1 = new Scanner(System.in);
-
-                double f = scanner1.nextDouble();
-                double c;
-
-                c = (f - 32) * 5 / 9;
-
-                System.out.println(f + "F ------------------> " + c + "C");
-            }
-            //if the user does not choose the correct number
-            else {
-                boolean choix = true;
-                while(choix) {
-                    System.out.println("choose a correct number (1 OR 2):");
-                    choice = sc.nextInt();
-                    if (choice == 1 || choice ==2){
-                        choix=false;
-                    }
-                    else{
-                        choix=true;
-                    }
+            boolean wrongchoice = true;
+            do{
+                if (choice.equals("1")) {
+                    wrongchoice = false;
+                    System.out.println("Enter the temperature to convert:");
+                    Scanner scanner = new Scanner(System.in);
+                    double c = scanner.nextDouble();
+                    double f;
+                    f = (c * 9 / 5) + 32;
+                    System.out.println(c + "C ------------------> " + f + "F");
                 }
-            }
+                //if the user chooses fahrenheit-celsius he must type 2
+                else if (choice.equals("2")) {
+                    wrongchoice = false;
+                    System.out.println("Enter the temperature to convert:");
+                    Scanner scanner1 = new Scanner(System.in);
+                    double f = scanner1.nextDouble();
+                    double c;
+                    c = (f - 32) * 5 / 9;
+                    System.out.println(f + "F ------------------> " + c + "C");
+                }
+                //if the user does not choose the correct number
+                else {
+                    System.out.println("choose a correct number (1 OR 2):");
+                    choice = sc.nextLine();
+                    wrongchoice = true;
+                }
+            }while (wrongchoice == true);
 
             Scanner scanner1 = new Scanner(System.in);
             //ask the user if he wants to convert other temperatures
@@ -63,54 +50,39 @@ public class AssignmentTemperatureConverter {
             String convert = scanner1.nextLine();
             //if yes repeat the loop to make another conversion
             if (convert.equals("yes")||convert.equals("YES")||convert.equals("Yes")){
-                con = true;
+                convertyes = true;
                 //if "no" exit the loop and display "Good bye, see you soon"
             } else if (convert.equals("no")||convert.equals("NO")||convert.equals("No")) {
-                con = false;
+                convertyes = false;
                 System.out.println("-------------------------------Good bye, see you soon---------------------------");
             }
             //if the user is wrong display "The input value is not valid, please write (Yes OR No)" until he makes the right choice
             else{
-                boolean invalid = true;
-
-                while (invalid){
+                boolean inputinvalid = true;
+                while (inputinvalid){
                     System.out.println("The input value is not valid, please write (Yes OR No)");
                     convert = scanner1.nextLine();
                     switch (convert)
                     {
                         case "yes":
-                            con = true;
-                            invalid = false;
-                            break;
                         case "YES":
-                            con = true;
-                            invalid = false;
-                            break;
                         case "Yes":
-                            con = true;
-                            invalid = false;
+                            convertyes = true;
+                            inputinvalid = false;
                             break;
                         case "no":
-                            con = false;
-                            invalid = false;
-                            System.out.println("-------------------------------Good bye, see you soon---------------------------");
-                            break;
                         case "No":
-                            con = false;
-                            invalid = false;
-                            System.out.println("-------------------------------Good bye, see you soon---------------------------");
-                            break;
                         case "NO":
-                            con = false;
-                            invalid = false;
+                            convertyes = false;
+                            inputinvalid = false;
                             System.out.println("-------------------------------Good bye, see you soon---------------------------");
                             break;
                         default:
-                            invalid = true;
+                            inputinvalid = true;
                     }
                 }
             }
 
-        } while (con == true);
+        } while (convertyes == true);
     }
 }
